1d7da7955ca5156f33d547f5eb11c5c667022d62
mighty <mightymensa40@gmail.com>
2022-05-24 02:55:07 +0000

    bot created

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = process.env.BOT_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', (msg: any) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
